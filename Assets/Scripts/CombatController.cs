9b687a397415eac7e1d58b9e12c0f3c49f0b18ed
laox <laox@cs.washington.edu>
2018-11-08 16:55:10 -0800

    add basic rts control

---
+++ Assets/Scripts/CombatController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.XR.MagicLeap;
+using UnityEngine;
+
+
+public class CombatController : MonoBehaviour
+{
+    private MLInputController _controller;
+    private bool _enabled = false;
+    private bool _trigger = false;
+
+    public GameObject cloneObject;
+
+    void Awake()
+    {
+        MLInput.Start();
+        MLInput.OnControllerButtonUp += OnButtonUp;
+        _controller = MLInput.GetController(MLInput.Hand.Left);
+    }
+
+    void OnDestroy()
+    {
+        MLInput.OnControllerButtonUp -= OnButtonUp;
+        MLInput.Stop();
+    }
+    
+    void Update()
+    {
+        CheckControl();
+
+        if (_trigger && _enabled) {
+            // spawn new soldier
+            GameObject newObject = (GameObject) Instantiate(Resources.Load("./resource/enemy"));
+
+            // at some position of the island
+
+            newObject.transform.parent = GameObject.Find("GameObject3").transform;
+            newObject.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+
+            _trigger = false;
+        }
+
+    }
+
+    void CheckControl()
+    {
+        if (_controller.TriggerValue > 0.2f && _enabled)
+        {
+            _trigger = true;
+        }
+    }
+
+    // Home button to start game
+    void OnButtonUp(byte controller_id, MLInputControllerButton button)
+    {
+        if (button == MLInputControllerButton.HomeTap)
+        {
+            //_cube.SetActive(true);
+            //_cube.transform.position = transform.position + transform.forward * _distance;
+            //_cube.transform.rotation = transform.rotation;
+            _enabled = true;
+        }
+    }
+
+}
